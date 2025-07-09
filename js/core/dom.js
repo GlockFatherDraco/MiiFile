0f249c1bf5cf558ce04c2cf17213c4df13bb5b7a
GlockFatherDraco <161503241+GlockFatherDraco@users.noreply.github.com>
2025-07-09 22:01:57 +0100

    Delete js directory
    Signed-off-by: GlockFatherDraco <161503241+GlockFatherDraco@users.noreply.github.com>

--- js/core/dom.js
+++
@@ -1,7 +0,0 @@
-// ┌─────────────┐
-// │ DOM Utils   │
-// └─────────────┘
-export const $ = (sel, ctx=document) => ctx.querySelector(sel);
-export const $$ = (sel, ctx=document) => Array.from(ctx.querySelectorAll(sel));
-export const on = (el, evt, fn, opts) => el.addEventListener(evt, fn, opts);
-export const ready = fn => document.readyState !== 'loading' ? fn() : document.addEventListener('DOMContentLoaded', fn); 
